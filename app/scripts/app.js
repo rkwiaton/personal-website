bc3d0359205cb1e722813c8ae462d18c5122c90f
Roman Kwiaton <romankwiaton@Romans-MBP.lan>
2016-01-03 20:24:35 +0000

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name personalWebsiteApp
+ * @description
+ * # personalWebsiteApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('personalWebsiteApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
